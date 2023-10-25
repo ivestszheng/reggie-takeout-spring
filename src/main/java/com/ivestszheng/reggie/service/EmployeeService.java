32b53587297854c85c1cb8023f8a93eec2e479d6
无声2017 <ivestszheng@qq.com>
2023-10-25 11:30:21 +0800

    feat(后台登录): 创建 controller,service,mapper,entity

---
+++ src/main/java/com/ivestszheng/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.ivestszheng.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ivestszheng.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
---
+++ src/main/java/com/ivestszheng/reggie/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.ivestszheng.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ivestszheng.reggie.entity.Employee;
+import com.ivestszheng.reggie.mapper.EmployeeMapper;
+import com.ivestszheng.reggie.service.EmployeeService;
+
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+}
